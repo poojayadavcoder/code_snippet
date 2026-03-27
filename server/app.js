a28f2debc3673bb1dc9dd5f8df5c565711eaa122
Pooja <py5825590@gmail.com>
2026-03-27 16:07:02 +0530

    update

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import connectDB from './config/db.js'
 import snippetRoutes from './routes/snippetRoutes.js'
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 connectDB();
 
