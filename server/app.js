c3467b566794f084687e040f5de979d96be8178e
Pooja <py5825590@gmail.com>
2026-03-29 17:51:31 +0530

    fix origin

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: "http://localhost:5173", // Default Vite port
+  origin: process.env.VITE_BASE_URL,
   credentials: true
 }));
 app.use(cookieParser());
