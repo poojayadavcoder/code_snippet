2979a552b8985b216dadeab6aae51ff12da65dbf
Pooja <py5825590@gmail.com>
2026-03-30 17:59:18 +0530

    fix issue

--- server/app.js
+++ server/app.js
@@ -5,24 +5,34 @@ import connectDB from './config/db.js'
 import snippetRoutes from './routes/snippetRoutes.js'
 import authRoutes from './routes/authRoutes.js'
 import dotenv from "dotenv";
+
 dotenv.config();
+
 const app = express();
+
 connectDB();
+
+app.set('trust proxy', 1);
 
 app.use(cors({
   origin: process.env.VITE_BASE_URL,
   credentials: true
 }));
-app.use(cookieParser());
-app.set('trust proxy', 1);
+
 app.use(express.json());
+
+app.use(cookieParser());
+
+
 app.use("/api/auth", authRoutes);
+
 app.use("/api", snippetRoutes);
+
 app.get("/", (req, res) => {
   res.send("Snippet API Running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
