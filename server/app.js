b11cdafca579139691c389520579bb4fbaa64a52
Pooja <py5825590@gmail.com>
2026-03-30 17:21:03 +0530

    set rate limit

--- server/app.js
+++ server/app.js
@@ -14,6 +14,7 @@ app.use(cors({
   credentials: true
 }));
 app.use(cookieParser());
+app.set('trust proxy', 1);
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api", snippetRoutes);
