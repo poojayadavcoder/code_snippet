49875b99cfba02cd372bf765cb25b40128344bda
Pooja <py5825590@gmail.com>
2026-03-30 17:34:42 +0530

    fix bug

--- client/lib/api.js
+++ client/lib/api.js
@@ -10,19 +10,25 @@ api.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
 
-    if (error.response.status === 401 && !originalRequest._retry) {
+    if (
+      error.response.status === 401 &&
+      !originalRequest._retry &&
+      !originalRequest.url.includes("refresh-token") // ✅ Bug 2 fix
+    ) {
       originalRequest._retry = true;
 
       await axios.post(
-        `${import.meta.env.VITE_BASE_URL}/auth/refresh`,
+        `${import.meta.env.VITE_BASE_URL}/auth/refresh-token`, // ✅ Bug 1 fix
         {},
         { withCredentials: true }
       );
 
+      originalRequest.withCredentials = true; // ✅ Bug 3 fix
       return api(originalRequest);
     }
 
     return Promise.reject(error);
   }
 );
+
 export default api;
