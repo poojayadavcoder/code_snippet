89e63315e562dda5e77da00af1f626aa007ba7f7
Pooja <py5825590@gmail.com>
2026-04-07 17:45:57 +0530

    fix

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+// const socket = io("http://localhost:5000");
+const socket=io(`https://code-snippet-cdmx.onrender.com`)
 
 socket.on("connect", () => {
   console.log("Connected to socket:", socket.id);
